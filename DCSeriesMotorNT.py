f6a18d0e1b36ff1e2df3f034cea3de4188991ad0
mihirjuee <mihir.juee@gmail.com>
2026-04-18 19:32:24 +0530

    Update DCSeriesMotorNT.py

--- DCSeriesMotorNT.py
+++ DCSeriesMotorNT.py
@@ -28,9 +28,9 @@ with col1:
     d += elm.Resistor().label("Ra")
     d += elm.Motor().label("M")
     
-    d += elm.Line().down(length=2)
-    d += elm.Line().left(length=2)
-    
+    d += elm.Line().down(length=4)
+    d += elm.Line().left(length=6)
+    d += elm.Line().up(length=2)
     st.pyplot(d.draw().fig)
 
 
