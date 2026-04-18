bebb4176069d5faecc5d4173f0046284f32a26ba
mihirjuee <mihir.juee@gmail.com>
2026-04-19 00:02:44 +0530

    Update DC_Shunt_OCC.py

--- DC_Shunt_OCC.py
+++ DC_Shunt_OCC.py
@@ -51,4 +51,5 @@ st.markdown("""
 2.  **Field Connection:** The field winding must be connected such that the field current **aids** the residual flux.
 3.  **Critical Resistance:** The field resistance ($R_f$) must be **less than** the critical resistance ($R_c$). If $R_f > R_c$, the resistance line will not intersect the OCC, and voltage will not build up.
 4.  **Speed of Generator:** The speed of the prime mover must be greater than the critical speed, which is the minimum speed below which the generator cannot build up voltage.
+)
 
