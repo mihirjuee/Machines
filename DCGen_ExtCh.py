49be7d6f17f5764d891da9cb5f9f159905342427
mihirjuee <mihir.juee@gmail.com>
2026-04-19 20:32:51 +0530

    Update DCGen_ExtCh.py

--- DCGen_ExtCh.py
+++ DCGen_ExtCh.py
@@ -33,7 +33,7 @@ V = k * phi * N
 # ================= CIRCUITS =================
 
 def separately_excited():
-    d = schemdraw.Drawing(unit=1.5)
+    d = schemdraw.Drawing()
     
     
     d += elm.Line().up(0.1)
