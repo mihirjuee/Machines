a407ed0a913081c0b0a2556da41fd1436d7e730f
mihirjuee <mihir.juee@gmail.com>
2026-04-20 19:26:48 +0530

    Change sidebar header from 'Select Realistic Value' to 'Set Value'

--- EqCktIM.py
+++ EqCktIM.py
@@ -51,7 +51,7 @@ st.subheader("Torque-Speed Curve with Operating Point")
 # =========================
 # SIDEBAR
 # =========================
-st.sidebar.header("⚙️ Motor Parameters(Select Realistic Value)")
+st.sidebar.header("⚙️ Motor Parameters(Set Value)")
 
 V_line = st.sidebar.slider("Line Voltage (V)", 200, 500, 400)
 f = st.sidebar.slider("Frequency (Hz)", 40, 60, 50)
